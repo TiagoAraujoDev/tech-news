7c4f949ae05bdc98a6eeb7084fb13b7d5a8da7a2
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2022-11-24 15:02:00 +0000

    add: Home

---
+++ src/pages/components/SubscribeButton/index.tsx
@@ -0,0 +1,5 @@
+import styles from "./styes.module.scss";
+
+export const SubscribeButton = () => {
+  return <button className={styles.subscribeButton}>Subscribe now</button>;
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,24 @@
+import Image from "next/image";
+
+import { SubscribeButton } from "./components/SubscribeButton";
+
+import styles from "../styles/pages/home.module.scss";
+
 export default function Home() {
-  return <h1>Hello world!</h1>;
+  return (
+    <main className={styles.homeContainer}>
+      <section className={styles.heroSection}>
+        <span>👏 Hey, welcome!</span>
+        <h1>
+          News about the <span>React</span> world
+        </h1>
+        <p>
+          Get access to all publications <br />
+          <span>for $9.90 month</span>
+        </p>
+        <SubscribeButton />
+      </section>
+      <Image src="/woman.png" width={334} height={520} priority alt="" />
+    </main>
+  );
 }
