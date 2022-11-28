0ee19425de57042c31c944312b5851687a815b92
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2022-11-28 22:04:42 +0000

    feat: Add user to db

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,14 @@
+import { User } from "next-auth";
+import { AdapterUser } from "next-auth/adapters";
 import NextAuth from "next-auth/next";
 import GithubProvider from "next-auth/providers/github";
+import { query as q } from "faunadb";
+
+import { fauna } from "../../../lib/fauna";
+
+interface SignIn {
+  user: User | AdapterUser;
+}
 
 export const authOptions = {
   providers: [
@@ -12,7 +21,30 @@ export const authOptions = {
         }
       }
     })
-  ]
+  ],
+  callbacks: {
+    async signIn({ user }: SignIn): Promise<boolean> {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email!))
+              )
+            ),
+            q.Create(q.Create(q.Collection("users"), { data: { email } })),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email!)))
+          )
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+    }
+  }
 };
 
 export default NextAuth(authOptions);
