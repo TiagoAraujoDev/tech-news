232d621373f67c481990fbe33842d99069a1df27
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2023-04-18 15:22:37 +0100

    feat: Copy button in code snippets

--- slices/CodeBlock/index.tsx
+++ slices/CodeBlock/index.tsx
@@ -2,6 +2,7 @@ import { SliceComponentProps } from "@prismicio/react";
 import { Content } from "@prismicio/client";
 import { useEffect, useRef, useState } from "react";
 import hljs from "highlight.js";
+import { Clipboard } from "phosphor-react";
 
 import style from "../../src/styles/slices/CodeBlock.module.scss";
 
@@ -17,16 +18,24 @@ type CodeBlockProps = SliceComponentProps<CodeBlockSlice>;
 const CodeBlock = ({ slice }: CodeBlockProps) => {
   const [copy, setCopy] = useState(false);
   const codeRef = useRef(null);
-  
+
   const lang = slice.primary.lang as string;
 
   useEffect(() => {
     hljs.highlightAll();
   }, []);
 
+  const handleCopyText = () => {
+    setCopy(true);
+    navigator.clipboard.writeText(slice.primary.code!);
+  };
+
   return (
     <section className={style.container}>
-      <pre className={`hljs ${lang}` } ref={codeRef}>
+      <button onClick={handleCopyText} className={style.clipboardBtn}>
+        {copy ? <span>copied!</span> : <Clipboard size={24} color="#fff" />}
+      </button>
+      <pre className={`hljs ${lang}`} ref={codeRef}>
         <code>{slice.primary.code}</code>
       </pre>
     </section>
